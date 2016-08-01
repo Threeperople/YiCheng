be3b528a18e8b75dcc676ad4fa6a10a843e1e451
光光 <348697233@qq.com>
2016-08-01 20:44:12 +0800

    导包

---
+++ app/src/main/java/com/example/administrator/yicheng/blogdayf/BlogdayFragment.java
@@ -0,0 +1,25 @@
+package com.example.administrator.yicheng.blogdayf;
+
+
+import com.example.administrator.yicheng.R;
+import com.example.administrator.yicheng.base.BaseFragment;
+
+
+public class BlogdayFragment extends BaseFragment implements BlogdayContract.View{
+
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.fragment_blogday;
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void initData() {
+
+    }
+}
---
+++ app/src/main/java/com/example/administrator/yicheng/blogdayf/BlogdayPresenter.java
@@ -0,0 +1,8 @@
+package com.example.administrator.yicheng.blogdayf;
+
+/**
+ * Created by Jensen on 2016/8/1.
+ */
+public class BlogdayPresenter implements BlogdayContract.Presenter {
+
+}
