21ea8a2e9d3b14247eb84713226cfc216c722823
刘迅 <540278292@qq.com>
2016-08-02 12:56:19 +0800

    第五次上传,分包

--- app/src/main/java/com/example/administrator/yicheng/main/MainPresenter.java
+++ app/src/main/java/com/example/administrator/yicheng/main/MainPresenter.java
@@ -5,6 +5,6 @@ import com.example.administrator.yicheng.main.minef.MineContract;
 /**
  * Created by Administrator on 2016/8/1.
  */
-public class MainPresenter implements MineContract.Presenter {
+public class MainPresenter implements MainContract.Presenter {
 
 }
