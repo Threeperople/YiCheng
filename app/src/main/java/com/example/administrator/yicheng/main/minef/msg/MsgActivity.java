98d3813e1c6151c426eb3884befeb11507abcc00
刘迅 <540278292@qq.com>
2016-08-03 18:49:45 +0800

    第八次上传，设置msg,store空视图

--- app/src/main/java/com/example/administrator/yicheng/main/minef/msg/MsgActivity.java
+++ app/src/main/java/com/example/administrator/yicheng/main/minef/msg/MsgActivity.java
@@ -1,12 +1,25 @@
 package com.example.administrator.yicheng.main.minef.msg;
 
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.ListView;
+
 import com.example.administrator.yicheng.R;
 import com.example.administrator.yicheng.base.BaseActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by Administrator on 2016/8/2.
  */
 public class MsgActivity extends BaseActivity {
+    @BindView(R.id.msg_toolBarIcon)
+    ImageView msgToolBarIcon;
+    @BindView(R.id.msg_listView)
+    ListView msgListView;
+
     @Override
     public int getLayoutId() {
         return R.layout.activity_msg;
@@ -15,11 +28,17 @@ public class MsgActivity extends BaseActivity {
 
     @Override
     public void initView() {
-
+        msgListView.setEmptyView(findViewById(R.id.msg_emptyView));
     }
 
     @Override
     public void initData() {
+
+    }
+
+
+    @OnClick(R.id.msg_toolBarIcon)
+    public void onClick() {
 
     }
 }
--- app/src/main/java/com/example/administrator/yicheng/main/minef/store/StoreActicity.java
+++ app/src/main/java/com/example/administrator/yicheng/main/minef/store/StoreActicity.java
@@ -1,12 +1,26 @@
 package com.example.administrator.yicheng.main.minef.store;
 
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.ListView;
+
 import com.example.administrator.yicheng.R;
 import com.example.administrator.yicheng.base.BaseActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by Administrator on 2016/8/3.
  */
 public class StoreActicity extends BaseActivity {
+
+    @BindView(R.id.store_toolBarIcon)
+    ImageView storeToolBarIcon;
+    @BindView(R.id.store_listView)
+    ListView storeListView;
+
     @Override
     public int getLayoutId() {
         return R.layout.activity_store;
@@ -15,11 +29,18 @@ public class StoreActicity extends BaseActivity {
 
     @Override
     public void initView() {
-
+        storeListView.setEmptyView(findViewById(R.id.store_emptyView));
     }
 
     @Override
     public void initData() {
 
+    }
+
+
+
+
+    @OnClick(R.id.store_toolBarIcon)
+    public void onClick() {
     }
 }
