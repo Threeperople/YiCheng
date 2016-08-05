2397b2e135b130c23f230fb6d1785659cbfa7b19
光光 <348697233@qq.com>
2016-08-05 11:10:41 +0800

    地图定位和分享

--- app/src/main/java/com/example/administrator/yicheng/main/MainActivity.java
+++ app/src/main/java/com/example/administrator/yicheng/main/MainActivity.java
@@ -7,6 +7,10 @@ import android.widget.RadioGroup;
 import com.example.administrator.yicheng.FragmentFactory;
 import com.example.administrator.yicheng.R;
 import com.example.administrator.yicheng.base.BaseActivity;
+import com.example.administrator.yicheng.main.Read.ReadFragment;
+import com.example.administrator.yicheng.main.blogdayf.BlogdayFragment;
+import com.example.administrator.yicheng.main.minef.MineFragment;
+import com.example.administrator.yicheng.main.profilef.ProfileFragment;
 
 import butterknife.BindView;
 
@@ -14,6 +18,10 @@ public class MainActivity extends BaseActivity implements MainContract.View {
 
     @BindView(R.id.the_main_activity_RadioGroup)
     RadioGroup theMainActivityRadioGroup;
+    private BlogdayFragment blogdayFragment;
+    private ReadFragment readFragment;
+    private ProfileFragment profileFragment;
+    private MineFragment mineFragment;
 
     @Override
     public int getLayoutId() {
@@ -22,38 +30,40 @@ public class MainActivity extends BaseActivity implements MainContract.View {
 
     @Override
     public void initView() {
-
-        getSupportFragmentManager().beginTransaction().replace(R.id.the_main_activity_LinearLayoutContainer,
-                FragmentFactory.creatFragment(FragmentFactory.Blogday_fragment)).commit();
-
-
+        blogdayFragment = (BlogdayFragment) FragmentFactory.creatFragment(FragmentFactory.Blogday_fragment);
+        readFragment = (ReadFragment) FragmentFactory.creatFragment(FragmentFactory.Read_fragment);
+        profileFragment = (ProfileFragment) FragmentFactory.creatFragment(FragmentFactory.Profile_fragment);
+        mineFragment = (MineFragment) FragmentFactory.creatFragment(FragmentFactory.Activity_fragment);
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.the_main_activity_LinearLayoutContainer,
+                blogdayFragment).add(R.id.the_main_activity_LinearLayoutContainer,
+                readFragment).add(R.id.the_main_activity_LinearLayoutContainer,
+                profileFragment).add(R.id.the_main_activity_LinearLayoutContainer,
+                mineFragment);
+               fragmentTransaction.hide(readFragment).hide(profileFragment).hide(mineFragment).commit();
         theMainActivityRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
-                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
                 switch (checkedId) {
                     //第一页
                     case R.id.main_RadioGroup_one_blog_day:
-                        transaction.replace(R.id.the_main_activity_LinearLayoutContainer,
-                                FragmentFactory.creatFragment(FragmentFactory.Blogday_fragment));
+                        fragmentTransaction.hide(readFragment).hide(profileFragment).hide(mineFragment).show(blogdayFragment);
                         break;
-                    //第二页
+                    //第二
                     case R.id.main_RadioGroup_two_reading:
-                        transaction.replace(R.id.the_main_activity_LinearLayoutContainer,
-                                FragmentFactory.creatFragment(FragmentFactory.Read_fragment));
+                        fragmentTransaction.hide(blogdayFragment).hide(profileFragment).hide(mineFragment).show(readFragment);
                         break;
                     //第三页
                     case R.id.main_RadioGroup_three_profile:
-                        transaction.replace(R.id.the_main_activity_LinearLayoutContainer,
-                                FragmentFactory.creatFragment(FragmentFactory.Profile_fragment));
+                        fragmentTransaction.hide(blogdayFragment).hide(readFragment).hide(mineFragment).show(profileFragment);
                         break;
                     //第四页
                     case R.id.main_RadioGroup_four_mine:
-                        transaction.replace(R.id.the_main_activity_LinearLayoutContainer,
-                                FragmentFactory.creatFragment(FragmentFactory.Activity_fragment));
+                        fragmentTransaction.hide(blogdayFragment).hide(profileFragment).hide(readFragment).show(mineFragment);
                         break;
                 }
-                transaction.commit();
+                fragmentTransaction.commit();
             }
         });
     }
